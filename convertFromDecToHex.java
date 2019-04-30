fa0fa7fc5995473639798de6de2fc66c921b1e17
boanergues <kouadiohermann6@gmail.com>
2019-04-30 16:38:21 +0000

    Create convertFromDecToHex.java

---
+++ convertFromDecToHex.java
@@ -0,0 +1,40 @@
+private static String fromDecToHex(int in) {
+		String res = "", rDigit = "";
+		int q = in, r = 0, base = 16;
+		do {
+			q = in / base;
+			r = in % base;
+			// cast r value
+			rDigit = String.valueOf(r);
+			if (r > 9 && r < 16) {
+				for (String[] arr : HEX) {
+					if (arr[1].equals(String.valueOf(rDigit))) {
+						rDigit = arr[0];
+						break;
+					}
+				}
+			}
+			if (q >= 10 && q < base) {
+				for (String[] arr : HEX) {
+					if (arr[1].equals(String.valueOf(q))) {
+						res += arr[0];
+						if (r < base) {
+							res = rDigit + res;
+						}
+						break;
+					}
+				}
+			} else if (q < 10) {
+				res = "" + q + rDigit + res;
+			} else {
+				res = "" + rDigit + res;
+			}
+			if (q > base) {
+				in = q;
+			} else {
+				break;
+			}
+			// res = reverse(res);
+		} while (r != 0);
+		return writeOntwoDigit(res);
+	}
